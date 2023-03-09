b31de178f2cfbbc49ea21032e184fa945993ed88
AZURIIEAL <abinbinu678@gmail.com>
2023-03-09 13:07:22 +0530

    Theme Settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import React, { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { themeSettings } from "./theme";
+import createTheme from "@mui/material";
+import { useSelector } from "react-redux";
+
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+
+  //Setting up the theme.Material Ui has a createTheme function that takes in a theme object and returns a theme object.
+  const mode = useSelector((state) => state.global.mode); //Way to grab states that are in the global slice.
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //Way to create a theme based on the mode.
+  return <div className="app">
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  </div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+//Imports for Redux.
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from './state/globalSlice';
+import { Provider } from 'react-redux';
 
+const store = configureStore({
+  reducer: {
+    global: globalReducer,}
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //Wrapping the App component with the Provider component.
   <React.StrictMode>
+    <Provider store={store}> 
     <App />
+    </Provider>
   </React.StrictMode>
 );
